6a4b5b3883aa2b7e1f4f33febd79439b96c51b77
Mario Sanchez Palma <m.sanchez.palma@gmail.com>
2021-05-24 15:41:44 +0200

    PSR-4 --> new namespace LaSalle\GroupSeven created

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace LaSalle\GroupSeven\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 
--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace LaSalle\GroupSeven;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
